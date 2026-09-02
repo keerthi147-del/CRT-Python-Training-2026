e34aa7c424e12a60431e676c6660519918526e08
keerthi147-del <y.keerthi152006@gmail.com>
2026-09-02 14:10:42 +0530

    Add Day 1 Python programs

---
+++ Day-01/programs.py
@@ -0,0 +1,18 @@
+# CRT Python Training - Day 01
+# Date: 31 August 2026
+
+# 1. Printing a simple message
+print("Hello, Python!")
+
+# 2. Printing a string
+print("Welcome to CRT Python Training")
+
+# 3. Using single quotes
+print('Python is easy to learn')
+
+# 4. Using double quotes
+print("Python is easy to learn")
+
+# 5. Using triple quotes
+print("""This is a
+multi-line string""")
